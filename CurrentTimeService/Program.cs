366433ceb862ebc940e45a0145ea4a28accffc70
yogi <yogi@gmail.cOm>
2024-02-26 22:19:11 +0530

    push qa

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -14,3 +14,30 @@ app.UseHttpsRedirection();
 app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
 
 await app.RunAsync();
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        string userInput = Console.ReadLine();
+
+        if (double.TryParse(userInput, out double number))
+        {
+            double result = SquareRootOfNumber(number);
+            Console.WriteLine($"Square root of {number} is: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+
+    static double SquareRootOfNumber(double x)
+    {
+        return Math.Sqrt(x);
+    }
+}
